2074759e3af4db8910cd10b9cbaa8c2e38f74930
Your Name <you@example.com>
2020-03-07 23:17:16 +0800

    增加glide依赖和camera调用

--- app/src/main/java/com/example/shotmarker/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/shotmarker/ui/dashboard/DashboardFragment.java
@@ -1,6 +1,13 @@
 package com.example.shotmarker.ui.dashboard;
 
+import android.Manifest;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,20 +20,30 @@ import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.navigation.Navigation;
 
+import com.bumptech.glide.Glide;
+import com.example.shotmarker.MainActivity;
 import com.example.shotmarker.R;
 import com.google.android.material.navigation.NavigationView;
 
+import java.io.File;
+import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
+
+import pub.devrel.easypermissions.EasyPermissions;
+
+import static android.app.Activity.RESULT_OK;
 
 public class DashboardFragment extends Fragment {
 
     private DashboardViewModel dashboardViewModel;
+    private String[] permissions = {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -43,6 +60,52 @@ public class DashboardFragment extends Fragment {
         return root;
     }
 
+    //获取权限
+    private void getPermission() {
+        if (EasyPermissions.hasPermissions(getActivity(), permissions)) {
+            //已经打开权限
+            Toast.makeText(getActivity(),"已申请",Toast.LENGTH_SHORT);
+        } else {
+            //没有打开相关权限、申请权限
+            EasyPermissions.requestPermissions(this, "需要获取您的相册、照相使用权限", 1, permissions);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        //框架要求必须这么写
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
+    }
+    //成功打开权限
+    public void onPermissionsGranted(int requestCode, @NonNull List<String> perms) {
+
+        Toast.makeText(getActivity(), "相关权限获取成功", Toast.LENGTH_SHORT).show();
+    }
+    //用户未同意权限
+    public void onPermissionsDenied(int requestCode, @NonNull List<String> perms) {
+        Toast.makeText(getActivity(), "请同意相关权限，否则功能无法使用", Toast.LENGTH_SHORT).show();
+    }
+
+    private File cameraSavePath;//拍照照片路径
+    private Uri uri;//照片uri
+
+    //激活相机操作
+    private void goCamera() {
+        cameraSavePath = new File(Environment.getExternalStorageDirectory().getPath() + "/" + System.currentTimeMillis() + ".jpg");
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            //第二个参数为 包名.fileprovider
+            uri = FileProvider.getUriForFile(getContext(), "com.example.hxd.pictest.fileprovider", cameraSavePath);
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        } else {
+            uri = Uri.fromFile(cameraSavePath);
+        }
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+        getActivity().startActivityForResult(intent, 1);
+    }
+
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
@@ -54,6 +117,7 @@ public class DashboardFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 Toast.makeText(getActivity(),"clicked",Toast.LENGTH_SHORT).show();
+                goCamera();
             }
         });
 
